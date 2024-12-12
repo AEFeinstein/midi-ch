b060810c0788ec0c1b8edb57633ca8c7fe051ba1
gelakinetic <gelakinetic@gmail.com>
2024-12-12 01:02:52 +0000

    Update Bleed & Race

--- do_everything.sh
+++ do_everything.sh
@@ -2,7 +2,7 @@
 
 # Make names swadge friendly
 mv "./midis/Carrie Wood -Swadge-Ocean-City-1989-SwadgeHero.mid" "./midis/sh_ocean.mid"
-mv "./midis/Copy of ChowaRaceFinal.mid" "./midis/sh_crace.mid"
+mv "./midis/Tuck - ChowaRaceFinal with note map.mid" "./midis/sh_crace.mid"
 mv "./midis/JnWake - Wake Man Stage.mid" "./midis/sh_wakeman.mid"
 mv "./midis/Mattmattat - The Dance of the Cremulons Final.mid" "./midis/sh_cremulons.mid"
 mv "./midis/Meldecision - the devils lullaby updated.mid" "./midis/sh_devils.mid"
@@ -11,7 +11,7 @@ mv "./midis/Newmajoe - Hot Dog Credits.mid" "./midis/sh_credits.mid"
 mv "./midis/Newmajoe - Pango.mid" "./midis/sh_pango.mid"
 mv "./midis/Rampey - Pain Final With Beatmap.mid" "./midis/sh_pain.mid"
 mv "./midis/Rampey - Revenge BeatmapFix.mid" "./midis/sh_revenge.mid"
-mv "./midis/TheManPF - Let it Bleed.mid" "./midis/sh_bleed.mid"
+mv "./midis/TheManPF - let it bleed notemap_16.mid" "./midis/sh_bleed.mid"
 
 # Chart everything
 source .venv/bin/activate
