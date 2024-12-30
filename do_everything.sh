15474f517edcef66c6c0333d8902dc077f4b32c5
gelakinetic <gelakinetic@gmail.com>
2024-12-30 23:49:26 +0000

    Latest midis

--- do_everything.sh
+++ do_everything.sh
@@ -5,7 +5,7 @@ mv "./midis/Carrie Wood -Ocean-City-1989 (fixed bass).mid" "./midis/sh_ocean.mid
 mv "./midis/Tuck - Chowa Race (Final 12 16).mid" "./midis/sh_crace.mid"
 mv "./midis/JnWake - Wake Man Stage.mid" "./midis/sh_wakeman.mid"
 mv "./midis/Mattmattat - Dance of the Creumlons (USE THIS).mid" "./midis/sh_cremulons.mid"
-mv "./midis/Meldecision - devil_s lullaby (jn fixed again 12 27).mid" "./midis/sh_devils.mid"
+mv "./midis/Meldecision - The Devil_s Lullaby (FINAL FOR REAL).mid" "./midis/sh_devils.mid"
 mv "./midis/Newmajoe - Chowa Grove.mid" "./midis/sh_cgrove.mid"
 mv "./midis/Newmajoe - Hot Dog Credits.mid" "./midis/sh_credits.mid"
 mv "./midis/Newmajoe - Pango (simplified 12.14).mid" "./midis/sh_pango.mid"
